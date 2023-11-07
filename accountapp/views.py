b9ecc96978de221f7c551adf90ed13f2e83cac88
서동우 <2tiny2@naver.com>
2023-11-07 11:01:13 +0000

    첫 커밋 11/7

---
+++ accountapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def hello_world(request):
+    
+    if request.method == "POST":
+        return render(request, 'accountapp/hello_world.html', context={'text': 'POST METHOD'})
+    else:
+        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD'})
+        
+        
