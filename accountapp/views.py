e56db56707411b98db6bf0de2eba99613c444ef1
서동우 <2tiny2@naver.com>
2023-11-10 11:16:05 +0000

    26강 섹션2 Account app 완료

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-
+from accountapp.templates.accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView, TemplateView, UpdateView, DeleteView
 
 def hello_world(request):
     
@@ -29,3 +29,19 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
+    
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'accountapp/detail.html'
+    
+class AccountUpdateView(UpdateView):
+    model = User 
+    form_class = AccountUpdateForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/update.html'
+    
+class AccountDeleteView(DeleteView):
+    model = User
+    success_url = reverse_lazy('accountapp:login')
+    template_name = 'accountapp/delete.html'
