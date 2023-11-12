d96b00a9ebf9c6a197eb171de70c50cbaa97be76
서동우 <2tiny2@naver.com>
2023-11-12 01:14:16 +0000

    33강 프로필 앱 완료

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap4',
+    'profileapp',
 ]
 
 MIDDLEWARE = [
@@ -139,3 +140,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
 LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
